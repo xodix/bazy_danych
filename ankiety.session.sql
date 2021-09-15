4f37fcb013e591a5bbcfdec40fa927ee1e778009
xodix <kodmode@wp.pl>
2021-09-15 13:01:36 +0200

    finished

--- ankiety.session.sql
+++ ankiety.session.sql
@@ -25,3 +25,66 @@ FROM dane_ankiet
 WHERE srod_lok = 'rower'
 	AND pora_roku = 'wiosna';
 -- @block 5
+SELECT Wojewodztwo,
+	COUNT(*)
+FROM dane_ankiet
+Group By Wojewodztwo;
+-- @block 6
+Select Wyksztalcenie,
+	AVG(Dochod)
+FROM dane_ankiet
+GROUP BY Wyksztalcenie;
+-- @block 7
+Select Wojewodztwo,
+	MAX(Dochod) as najwyzszy
+FROM dane_ankiet
+Group By Wojewodztwo
+HAVING najwyzszy > 5000;
+-- @block 8
+SELECT COUNT(*)
+FROM zainteresowania
+	INNER JOIN lokomocja on zainteresowania.id = lokomocja.id_a
+WHERE zainteresowania.zainteresowanie = 'matematyka'
+	AND lokomocja.srod_lok = 'autobus'
+	AND lokomocja.pora_roku = 'jesien';
+-- @block 9
+SELECT COUNT(*) as kobiety,
+	(la.l - COUNT(*)) as mezczyzni
+FROM dane_ankiet,
+	(
+		SELECT COUNT(*) as l
+		FROM dane_ankiet
+	) as la
+WHERE Imie LIKE '%a';
+-- @block 10
+SELECT lokomocja.srod_lok,
+	dane_ankiet.Wojewodztwo,
+	COUNT(dane_ankiet.id)
+FROM lokomocja
+	Inner join dane_ankiet on lokomocja.id_a = dane_ankiet.id
+WHERE dane_ankiet.Wojewodztwo = 'Mazowieckie'
+	AND lokomocja.pora_roku = 'lato'
+GROUP BY lokomocja.srod_lok;
+-- @block 11
+SELECT COUNT(*),
+	Wojewodztwo
+FROM dane_ankiet
+GROUP BY Wojewodztwo
+HAVING COUNT(*) > 20;
+-- @block 12
+SELECT DISTINCT Imie,
+	Nazwisko,
+	Wojewodztwo
+FROM dane_ankiet
+	INNER JOIN zainteresowania USING(id)
+WHERE Wiek > 50
+	AND Wyksztalcenie IN ('wysze', 'srednie')
+	AND zainteresowanie NOT IN ('informatyka', 'gry komputerowe')
+ORDER BY Nazwisko;
+-- @block 13
+SeLECT AVG(Dochod) as srednia_dochodu
+from dane_ankiet
+	INNER join lokomocja ON dane_ankiet.id = lokomocja.id_a
+WHERE Imie LIKE '%a'
+	AND Wojewodztwo = 'Zachodniopomorskie'
+	AND srod_lok = 'rower'
