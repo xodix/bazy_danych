7b5f756d2791fcc2eca4e4a062ccc7ae18a52771
xodix <kodmode@wp.pl>
2021-10-13 13:09:04 +0200

    13/10/2021

--- ankiety.session.sql
+++ ankiety.session.sql
@@ -1,90 +1,29 @@
--- @block 1
-SELECT Imie,
-	Nazwisko
-FROM dane_ankiet
-WHERE Wiek < 25;
--- @block 2
-SELECT Nazwisko,
-	Imie,
-	Wyksztalcenie,
-	Wojewodztwo
-From dane_ankiet
-WHERE Wyksztalcenie = 'podstawowe'
-	AND Wojewodztwo IN ('Lubuskie', 'Lubelskie');
--- @block 3
-SELECT dane_ankiet.Imie,
-	dane_ankiet.Nazwisko
-FROM dane_ankiet
-	INNER JOIN zainteresowania USING(id)
-WHERE zainteresowania.zainteresowanie = 'matematyka';
--- @block 4
-SELECT dane_ankiet.Imie,
-	dane_ankiet.Nazwisko
-FROM dane_ankiet
-	INNER JOIN lokomocja on dane_ankiet.id = lokomocja.id_a
-WHERE srod_lok = 'rower'
-	AND pora_roku = 'wiosna';
--- @block 5
-SELECT Wojewodztwo,
-	COUNT(*)
-FROM dane_ankiet
-Group By Wojewodztwo;
--- @block 6
-Select Wyksztalcenie,
-	AVG(Dochod)
-FROM dane_ankiet
-GROUP BY Wyksztalcenie;
--- @block 7
-Select Wojewodztwo,
-	MAX(Dochod) as najwyzszy
-FROM dane_ankiet
-Group By Wojewodztwo
-HAVING najwyzszy > 5000;
--- @block 8
-SELECT COUNT(*)
-FROM zainteresowania
-	INNER JOIN lokomocja on zainteresowania.id = lokomocja.id_a
-WHERE zainteresowania.zainteresowanie = 'matematyka'
-	AND lokomocja.srod_lok = 'autobus'
-	AND lokomocja.pora_roku = 'jesien';
--- @block 9
-SELECT COUNT(*) as kobiety,
-	(la.l - COUNT(*)) as mezczyzni
-FROM dane_ankiet,
-	(
-		SELECT COUNT(*) as l
-		FROM dane_ankiet
-	) as la
-WHERE Imie LIKE '%a';
--- @block 10
-SELECT lokomocja.srod_lok,
-	dane_ankiet.Wojewodztwo,
-	COUNT(dane_ankiet.id)
-FROM lokomocja
-	Inner join dane_ankiet on lokomocja.id_a = dane_ankiet.id
-WHERE dane_ankiet.Wojewodztwo = 'Mazowieckie'
-	AND lokomocja.pora_roku = 'lato'
-GROUP BY lokomocja.srod_lok;
--- @block 11
-SELECT COUNT(*),
-	Wojewodztwo
-FROM dane_ankiet
-GROUP BY Wojewodztwo
-HAVING COUNT(*) > 20;
--- @block 12
-SELECT DISTINCT Imie,
-	Nazwisko,
-	Wojewodztwo
-FROM dane_ankiet
-	INNER JOIN zainteresowania USING(id)
-WHERE Wiek > 50
-	AND Wyksztalcenie IN ('wysze', 'srednie')
-	AND zainteresowanie NOT IN ('informatyka', 'gry komputerowe')
-ORDER BY Nazwisko;
--- @block 13
-SeLECT AVG(Dochod) as srednia_dochodu
-from dane_ankiet
-	INNER join lokomocja ON dane_ankiet.id = lokomocja.id_a
-WHERE Imie LIKE '%a'
-	AND Wojewodztwo = 'Zachodniopomorskie'
-	AND srod_lok = 'rower'
+-- @block tworzenie_tabel
+CREATE TABLE `firma_2`.`pracownicy` (
+	`id_pracownika` INT NOT NULL AUTO_INCREMENT,
+	`nazwisko` VARCHAR(100) NOT NULL,
+	`imie` VARCHAR(100) NOT NULL,
+	PRIMARY KEY (`id_pracownika`)
+);
+CREATE TABLE `firma_2`.`place` (
+	`id_pracownika` INT NOT NULL,
+	`pensja` INT NOT NULL COMMENT 'Liczba PLN w groszach',
+	`dodatki` INT NOT NULL COMMENT 'Liczba PLN w groszach',
+	PRIMARY KEY (`id_pracownika`)
+);
+CREATE TABLE `firma_2`.`magazyn` (
+	`id_towaru` INT NOT NULL AUTO_INCREMENT,
+	`nazwa` VARCHAR(1000) NOT NULL,
+	`cena` INT NOT NULL COMMENT 'Cena w groszach',
+	`liczba_sztuk` INT NOT NULL,
+	PRIMARY KEY (`id_towaru`)
+);
+-- @block tworzenie_urzytkownikow
+CREATE User `kadrowa`;
+CREATE User `ksiegowa`;
+CREATE USER `magazynier`;
+CREATE USER `informatyk`;
+CREATE USER `prezes`;
+-- @block nadawanie_uprawnien
+Grant All ON firma_2.pracownicy TO `kadrowa`;
+GRANT SELECT on firma_2.place to `kadrowa`;
